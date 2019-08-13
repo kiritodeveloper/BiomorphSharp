371d1d7438145e5250ea29bb72f2f9f06d011384
Tom Padilla <padilla.henry@gmail.com>
2019-08-13 07:50:33 -0500

    Initial codebase

---
+++ BiomorphSharp/BiomorphSharp/MainController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace BiomorphSharp
+{
+    public class MainController
+    {
+        public Biomorph[,] Biomorph { get; set; }
+
+        public MainController()
+        {
+            Biomorph = new Biomorph[7,5];
+        }
+
+
+        public void Reproduce(Biomorph currentBiomorph)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Biomorph
+    {
+        public ImageBrush Brush { get; set; }
+
+        public Biomorph()
+        {
+            Pen drawPen = new Pen();
+            drawPen.Thickness = 2;
+            drawPen.Brush = Brushes.Red;
+
+            DrawingVisual visual = new DrawingVisual();
+            DrawingContext context = visual.RenderOpen();
+            context.DrawRectangle(null, drawPen, new Rect(0, 0, 30, 30));
+            context.Close();
+
+            // Create the Bitmap and render the rectangle onto it.
+            RenderTargetBitmap bmp = new RenderTargetBitmap(30, 30, 96, 96, PixelFormats.Pbgra32);
+            bmp.Render(visual);
+
+            Brush.ImageSource = bmp;
+            //BaseImage.Background = parentBrush;
+        }
+
+        private void Develop()
+        { }
+    }
+}
---
+++ BiomorphSharp/BiomorphSharp/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BiomorphSharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainController mainController;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainController = new MainController();
+
+            mainController.Reproduce(new Biomorph());
+            DisplayImages();
+        }
+
+        private void Biomorph_OnClick(object sender, RoutedEventArgs e)
+        {
+            mainController.Reproduce((Biomorph)((Button) sender).Tag);
+        }
+
+        private void DisplayImages()
+        {
+            Image00.Background = mainController.Biomorph[0,0].Brush;
+            Image00.Tag = mainController.Biomorph[0,0];
+
+            //Image10
+            //Image20
+            //Image40
+            //Image50
+            //Image60
+
+            //Image01
+            //Image61
+
+            //Image02
+            //Image62
+
+            //Image03
+            //Image63
+
+            //Image04
+            //Image14
+            //Image24
+            //Image44
+            //Image54
+            //Image64
+
+        }
+    }
+}
